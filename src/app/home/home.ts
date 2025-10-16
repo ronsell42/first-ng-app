3ea39d76783f9558dfbb1762f615a05ad98674f2
ronsell <ronsell42@gmail.com>
2025-10-17 05:35:12 +0800

    Added: Added inputs

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,16 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { Greeting } from '../components/greeting/greeting';
+import { Counter } from '../components/counter/counter';
 
 @Component({
   selector: 'app-home',
-  imports: [Greeting],
+  imports: [Greeting, Counter],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
   homeMessage = signal('Home Message');
 
-  keyUpHandler() {
-    console.log('user typed something in the input')
+  keyUpHandler(event: KeyboardEvent) {
+    const key = event.key;
+
+    (/^[a-zA-Z]$/.test(key)) ? console.log(`user typed ${event.key} in the input`)
+    : console.log(`Non Letter Key was pressed`)
   }
 }
