2fecd49eeaf78ad0a4a55adc64d164e5300f7bd7
ronsell <ronsell42@gmail.com>
2025-10-19 10:29:42 +0800

    Add: Search filtering and Pipes

--- src/app/components/todo-item/todo-item.ts
+++ src/app/components/todo-item/todo-item.ts
@@ -1,11 +1,12 @@
 import { Component, input, output } from '@angular/core';
 import { Todo } from '../../model/todo.type';
 import { HighlightCompletedTodo } from "../../directives/highlight-completed-todo";
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [HighlightCompletedTodo],
+  imports: [HighlightCompletedTodo, UpperCasePipe],
   templateUrl: './todo-item.html',
   styleUrls: ['./todo-item.scss']
 })
--- src/app/todos/todos.ts
+++ src/app/todos/todos.ts
@@ -3,16 +3,19 @@ import { TodoService } from '../services/todos';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
 import { TodoItemComponent } from '../components/todo-item/todo-item';
+import { FormsModule } from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos-pipe';
 
 @Component({
   selector: 'app-todos',
-  imports: [TodoItemComponent],
+  imports: [TodoItemComponent, FormsModule, FilterTodosPipe],
   templateUrl: './todos.html',
   styleUrl: './todos.scss'
 })
 export class Todos implements OnInit{
   todoService = inject(TodoService);
   myTodoItems = signal<Array<Todo>>([]);
+  searchTerm = signal('');
 
   ngOnInit(): void {
     this.todoService.getTodosFromAPI()
@@ -29,5 +32,19 @@ export class Todos implements OnInit{
           this.myTodoItems.set(todos);
         }
       )
+  }
+
+  updateTodoItem(todoItem: Todo){
+    this.myTodoItems.update((todos) => {
+      return todos.map(todo => {
+        if (todo.id === todoItem.id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo;
+      })
+    })
   }
 }
